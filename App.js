60bfc3adb023d611db02a9e0db762eb11e86bffb
Krasimir Dimitrov <133798641+krasi-pd-bg@users.noreply.github.com>
2026-01-11 16:15:13 +0200

    initial repo todo app

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.body}>
+
+            <View>
+                <Text>TODO List</Text>
+            </View>
+            <View>
+                <Text>Body</Text>
+            </View>
+            <View>
+                <Text>View</Text>
+            </View>
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = {
+    body: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: '#42a39e',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+};
