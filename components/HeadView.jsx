c45d0510d97fb31168a0025686ad461dc47eef7d
Krasimir Dimitrov <133798641+krasi-pd-bg@users.noreply.github.com>
2026-01-29 13:18:52 +0200

    AsyncStorage added

--- components/HeadView.jsx
+++ components/HeadView.jsx
@@ -1,17 +1,3 @@
-// import { ImageBackground, Text } from "react-native";
-
-// export default function HeadView() {
-//     return (
-
-//         <ImageBackground
-//             source={{ uri: 'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80' }}
-//             style={{ width: '100%', height: 150, opacity: 0.4 }}
-//         >
-//             <Text style={{ position: 'absolute', top: 20, color: 'white', fontSize: 18, fontWeight: 'bold' }}>Krasimir Dimitrov</Text>
-//         </ImageBackground>
-
-//     );
-// };
 import { memo } from 'react';
 import { ImageBackground, Text, StyleSheet } from 'react-native';
 
--- components/TodoList.jsx
+++ components/TodoList.jsx
@@ -1,14 +1,3 @@
-// import { ScrollView } from 'react-native';
-// import TodoItem from './TodoItem';
-
-// export default function TodoList({ todo, onDone, onDelete }) {
-//     return (
-//         <ScrollView style={{ flex: 1, width: '100%' }}>
-//             {todo.map((todoItem) => <TodoItem key={todoItem.id} item={todoItem} onDone={onDone} onDelete={onDelete}/>)}
-//         </ScrollView>
-//     );
-
-// };
 import { memo } from 'react';
 import { FlatList, StyleSheet } from 'react-native';
 import TodoItem from './TodoItem';
